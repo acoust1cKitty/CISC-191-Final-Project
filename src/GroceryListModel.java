70494751f3242763312417d86eeb2b53f00c4ed2
arnol <arnol@arnolde775-himalayas.nord>
2023-10-16 14:01:46 -0700

    Fixed the Build Path issue that was preventing the Model class from running.

--- src/GroceryListModel.java
+++ src/GroceryListModel.java
@@ -1,10 +1,13 @@
 
-public class GroceryListModel {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
-
+public class GroceryListModel 
+{
+	public GroceryListModel() 
+	{
+		
+		
 	}
-
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 }
